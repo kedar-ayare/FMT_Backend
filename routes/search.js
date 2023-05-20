e1827d6ca17d70db4769291a6ed82a4f7d5b968f
Kedar Ayare <kedarayareilr@gmail.com>
2023-05-20 19:19:51 +0530

    Made changes to search apis, added a new get api, added comments

--- routes/search.js
+++ routes/search.js
@@ -7,6 +7,12 @@ const Users = require("../models/User")
 const tokenVerify = require("../middlewares/auth");
 
 
+/*
+    '/' - POST
+    Route to get Search Results
+    Takes string in body, and uses it for pattern matching
+    for first and last name fields.
+*/
 router.post('/', tokenVerify, async (req, res) => {
     // console.log(req.body.name);
     // var users = await Users.find({ fname: { $regex: req.body.name, $options: 'i' } });
@@ -21,12 +27,27 @@ router.post('/', tokenVerify, async (req, res) => {
     res.send("Route to get search results")
 })
 
+
+
+/* 
+    '/add' - POST
+    Route to add a new user Id to the current user's searched list
+    Requires Id of User who needs to be added in body of request
+
+*/
 router.post('/add', tokenVerify, async (req, res) => {
     console.log(req.User);
     try {
         await Users.updateOne(
             { _id: req.User },
-            { $addToSet: { searched: req.body.profId } },
+            {
+                $addToSet: {
+                    searched: {
+                        $each: [req.body.profId],
+                        $slice: -10
+                    }
+                }
+            },
         );
         console.log("Added successfully")
     } catch (err) {
@@ -36,6 +57,12 @@ router.post('/add', tokenVerify, async (req, res) => {
     res.send("Add a profile to current User's Searched List");
 })
 
+
+/*
+    '/delete' - POST
+    Route to remove a user Id from current user's searched list
+    Rewuires Id of User who needs to be added in body of request
+*/
 router.post('/delete', tokenVerify, async (req, res) => {
     console.log(req.User);
     try {
@@ -48,6 +75,25 @@ router.post('/delete', tokenVerify, async (req, res) => {
         console.log(err)
     }
     res.send("Remove a profile to current User's Searched List");
+})
+
+
+
+/*
+    '/' - GET
+    Route to get details of all users in the current user's searched list
+*/
+router.get('/', tokenVerify, async (req, res) => {
+    var searchedUSers = [];
+    var user = await Users.findOne({
+        _id: req.User
+    })
+
+    for (var user of user["searched"]) {
+        const userDets = await Users.findOne({ _id: user })
+        searchedUSers.push(userDets);
+    }
+    res.send(searchedUSers)
 })
 
 module.exports = router

b41534e685af005e22b92f0475c0f38b230a72aa
kedar-ayare <kedarayareilr@gmail.com>
2025-02-27 18:41:05 +0530

    Modified posts path to rollback if error when uploading any of the selected images

--- routes/posts.js
+++ routes/posts.js
@@ -5,58 +5,76 @@ const multer = require('multer')
 const Posts = require("../models/Post")
 const tokenVerify = require("../middlewares/auth");
 
-const upload = multer({ dest: 'uploads' })
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const router = express.Router()
 
-router.post('/newPost/', upload.array('files', 10), async (req, res) => {
+router.post('/newPost/', upload.array('files', 10), tokenVerify, async (req, res) => {
     if (!req.files || req.files.length === 0) {
-        return res.status(400).send("No files uploaded.");
+        return res.status(400).send({err: "PostErr-02"});
     }
 
-    const uploadPromises = req.files.map(file => {
-
-        // upload params for the S3 bucket
-        const uploadParams = {
-            Bucket: "myfamilytree2000",
-            Key: file.originalname,
-            Body: file.buffer,
-            ContentType: file.mimetype,
-            ACL: 'public-read'
-        };
-
-        return new Promise((resolve, reject) => {
-            s3.upload(uploadParams, (err, data) => {
-                if (err) {
-                    console.error("Error:", err);
-                    reject(err);
-                } else {
-                    console.log("Upload Success:", data.Location);
-                    resolve(data.Location);
-                }
-            });
-        });
-    });
-
+    let newPost;
+    
     try {
-        const imageLinks = await Promise.all(uploadPromises);
 
-        const newPost = Posts({
+        newPost = new Posts({
             caption: req.body.caption,
-            images: imageLinks,
+            images: [],
             likes: [],
             comments: [],
             datetime: new Date(),
-            author: req.body.author
+            author: req.User
         });
 
         await newPost.save();
-        res.send({ "id": newPost._id });
+
+        const uploadPromises = req.files.map((file, index) => {
+            const uploadParams = {
+                Bucket: "myfamilytree2000",
+                Key: `${newPost._id}-${index}`,
+                Body: file.buffer,
+                ContentType: file.mimetype,
+                ACL: 'public-read'
+            };
+
+            return new Promise((resolve, reject) => {
+                s3.upload(uploadParams, (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data.Location);
+                    }
+                });
+            });
+        });
+
+        const uploadedImages = await Promise.all(uploadPromises);
+
+        newPost.images = uploadedImages;
+        await newPost.save();
+        res.send({ id: newPost._id });
+
     } catch (error) {
-        console.error("Error uploading images:", error);
-        res.status(500).send("Error uploading images.");
+        if (newPost?.images.length > 0) {
+            const deletePromises = newPost.images.map((imageUrl) => {
+                const key = imageUrl.split('/').pop(); // Extract filename from URL
+                return s3.deleteObject({ Bucket: "myfamilytree2000", Key: key }).promise();
+            });
+
+            await Promise.all(deletePromises);
+        }
+
+        if (newPost) {
+            await Posts.findByIdAndDelete(newPost._id);
+        }
+
+        res.status(500).send({err: "PostErr-03"});
     }
 });
+
+
 
 router.get('/', async (req, res) => {
     res.send("Get all posts")
@@ -77,8 +95,6 @@ router.get('/:id', tokenVerify, async (req, res) => {
         console.log(err)
         res.send({ err: "PostErr-01" })
     }
-
-
 })
 
 router.put('/:id', (req, res) => {
